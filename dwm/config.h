603a7474caa7abe59cde1aedd901b17f6d79a6d4
Matias Moya <akaihanatsundeee@proton.me>
2025-04-16 01:40:55 +0200

    modify and add keybindings, sort and organize code

--- dwm/config.h
+++ dwm/config.h
@@ -1,17 +1,17 @@
-/* See LICENSE file for copyright and license details. */
+/* akai's custom config */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "firacode:size=10" };
+static const char dmenufont[]       = "firacode:size=10";
+static const char col_gray1[]       = "#0f1015";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#ac3232"; /* focused color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "M", "A", "T", "I", "A", "S" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,8 +27,13 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+
+        /* class            instance    title  tags mask  isfloating  monitor */
+        { "firefox",        NULL,       NULL,  1 << 0,    0,          -1 },
+        { NULL,             "equibop",  NULL,  1 << 1,    0,          -1 },
+        { "prismlauncher",  NULL,       NULL,  1 << 2,    0,          -1 },
+        { "Emacs",          NULL,       NULL,  1 << 4,    0,          -1 },
+        { "corectrl",       NULL,       NULL,  1 << 5,    0,          -1 },
 };
 
 /* layout(s) */
@@ -60,41 +65,61 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
+// custom placeholders
+static const char *browser[]   = { "firefox", NULL };
+static const char *discord[]   = { "flatpak run io.github.equicord.equibop", NULL };
+static const char *minecraft[] = { "prismlauncher", NULL };
+static const char *music[]     = { "flatpak run com.github.th_ch.youtube_music", NULL };
+static const char *ddnet[]     = { "flatpak run tw.ddnet.ddnet.league.gctf", NULL };
+static const char *osu[]       = { "flatpak run sh.ppy.osu", NULL };
+
+/* bindkeys */
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	// workspaces (6)
+	TAGKEYS( XK_1, 0)
+	TAGKEYS( XK_2, 1)
+	TAGKEYS( XK_3, 2)
+	TAGKEYS( XK_q, 3)
+	TAGKEYS( XK_w, 4)
+	TAGKEYS( XK_e, 5)
+
+	// important dwm stuff
+	{ MODKEY,             XK_r,        spawn,            {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,   XK_Return,   quit,             {0} },
+
+	{ MODKEY,             XK_a,        spawn,  	     {.v = browser } },
+	{ MODKEY,             XK_s,        spawn,    	     {.v = discord } },
+	{ MODKEY,             XK_d,        spawn,  	     {.v = termcmd } },
+	{ MODKEY,             XK_f,        spawn,  	     {.v = music } },
+	{ MODKEY,             XK_g,        spawn,    	     {.v = minecraft } },
+	{ MODKEY,             XK_z,        spawn,    	     {.v = ddnet } },
+	{ MODKEY,             XK_x,        spawn,    	     {.v = osu } },
+
+	{ MODKEY,             XK_b,        togglebar,        {0} },
+	{ MODKEY,             XK_j,        focusstack,       {.i = +1 } },
+	{ MODKEY,             XK_k,        focusstack,       {.i = -1 } },
+
+	/* remove inc/dec master: i don't use it, takes up keybinding slots */
+	// { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	// { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	
+	// window control
+	{ MODKEY,             XK_h,        setmfact,         {.f = -0.05} },
+	{ MODKEY,             XK_l,        setmfact,         {.f = +0.05} },
+	{ MODKEY,             XK_Return,   zoom,             {0} },
+	{ MODKEY,             XK_Tab,      view,             {0} },
+	{ MODKEY|ShiftMask,   XK_c,        killclient,       {0} },
+	{ MODKEY,             XK_t,        setlayout,        {.v = &layouts[0]} },
+	{ MODKEY,             XK_f,        setlayout,        {.v = &layouts[1]} },
+	{ MODKEY,             XK_m,        setlayout,        {.v = &layouts[2]} },
+	{ MODKEY,             XK_space,    setlayout,        {0} },
+	{ MODKEY|ShiftMask,   XK_space,    togglefloating,   {0} },
+	{ MODKEY,             XK_0,        view,             {.ui = ~0 } },
+	{ MODKEY|ShiftMask,   XK_0,        tag,              {.ui = ~0 } },
+	{ MODKEY,             XK_comma,    focusmon,         {.i = -1 } },
+	{ MODKEY,             XK_period,   focusmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,   XK_comma,    tagmon,           {.i = -1 } },
+	{ MODKEY|ShiftMask,   XK_period,   tagmon,           {.i = +1 } },
 };
 
 /* button definitions */
